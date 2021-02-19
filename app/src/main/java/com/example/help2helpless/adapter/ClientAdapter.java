409b16c1c6a7d56c2dad14fb307df40e2c353a42
PHP <alamingazi68@gmail.com>
2021-02-19 22:37:40 +0600

    help to helpless update

--- app/src/main/java/com/example/help2helpless/adapter/ClientAdapter.java
+++ app/src/main/java/com/example/help2helpless/adapter/ClientAdapter.java
@@ -1,24 +1,40 @@
 package com.example.help2helpless.adapter;
 
+import android.app.AlertDialog;
 import android.content.Context;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.android.volley.AuthFailureError;
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
 import com.example.help2helpless.R;
 import com.example.help2helpless.model.Client;
+import com.example.help2helpless.model.Dealer;
 import com.squareup.picasso.Picasso;
+
+import org.json.JSONException;
+import org.json.JSONObject;
 
 import java.util.ArrayList;
 
 public class ClientAdapter extends RecyclerView.Adapter<ClientAdapter.ClientViewHolder> {
     ArrayList<Client> clientList;
-
+    View DialogueView;
+    AlertDialog dialog;
+    String url="https://apps.help2helpless.com/add_discount.php";
     public ClientAdapter(ArrayList<Client> clientList, Context context) {
         this.clientList = clientList;
         this.context = context;
@@ -33,12 +49,45 @@ public class ClientAdapter extends RecyclerView.Adapter<ClientAdapter.ClientView
     }
 
     @Override
-    public void onBindViewHolder(@NonNull ClientAdapter.ClientViewHolder holder, int position) {
+    public void onBindViewHolder(@NonNull ClientAdapter.ClientViewHolder holder, final int position) {
             Client client=clientList.get(position);
             holder.cname.setText(client.getcName());
             holder.address.setText(client.getCaddres());
         String imageUrl="https://apps.help2helpless.com/uploads/"+client.getCphoto();
         Picasso.get().load(imageUrl).resize(80,80).centerCrop().into(holder.client_image);
+        holder.itemView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Client client1=clientList.get(position);
+                createDialoge(client1.getCnumber());
+            }
+        });
+
+    }
+
+    public void   createDialoge(final String cnumber){
+        AlertDialog.Builder builder=new AlertDialog.Builder(context);
+        DialogueView=LayoutInflater.from(context).inflate(R.layout.add_discount,null);
+        Button button_ok= DialogueView.findViewById(R.id.selectok);
+        Button button_cancel=DialogueView.findViewById(R.id.selectcancel);
+        button_ok.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                addDiscount(cnumber) ;
+                dialog.dismiss();
+            }
+        });
+        button_cancel.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                dialog.dismiss();
+            }
+        });
+        builder.setView(DialogueView);
+        dialog= builder.create();
+        dialog.setCanceledOnTouchOutside(false);
+        dialog.show();
+
 
     }
 
@@ -46,7 +95,68 @@ public class ClientAdapter extends RecyclerView.Adapter<ClientAdapter.ClientView
     public int getItemCount() {
         return clientList.size();
     }
+    private void addDiscount(final String cnumber) {
 
+        StringRequest stringRequest=new StringRequest(Request.Method.POST, url, new com.android.volley.Response.Listener<String>() {
+            @Override
+            public void onResponse(String response) {
+                try {
+                    JSONObject jsonObject=new JSONObject(response);
+                    String result=jsonObject.getString("response");
+                    if(result.equals("success")){
+
+                        Toast.makeText(context,"Discount added Successfully",Toast.LENGTH_LONG).show();
+                        dialog.dismiss();
+                    }
+                    else{
+                        Toast.makeText(context," "+result,Toast.LENGTH_LONG).show();
+                    }
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+
+
+
+                ,new com.android.volley.Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                Toast.makeText(context,"not Uploaded",Toast.LENGTH_LONG).show();
+            }
+        })
+        {
+            @Override
+            public byte[] getBody() throws AuthFailureError {
+                JSONObject params = new JSONObject();
+                try {
+
+                    params.put("cl_contact","01354232516");
+                    params.put("dlr_contact","01629630487");
+                    params.put("month","july");
+                    params.put("year","2021");
+                    params.put("amount","210");
+
+
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+
+                return params.toString().getBytes();
+            }
+
+            @Override
+            public String getBodyContentType() {
+                return "application/json; charset=utf-8";
+            }
+
+        };
+
+        RequestQueue requestQueue= Volley.newRequestQueue(context);
+        requestQueue.add(stringRequest);
+
+
+    }
     public class ClientViewHolder extends RecyclerView.ViewHolder {
         TextView cname,address;
         ImageView client_image;
