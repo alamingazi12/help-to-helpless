6f302d5dd9b6ca49a9c1b943ac8df10bc2c41da1
PHP <alamingazi68@gmail.com>
2021-03-29 10:11:31 +0600

    help to helpless update

--- app/src/main/java/com/example/help2helpless/ClientProfileActivity.java
+++ app/src/main/java/com/example/help2helpless/ClientProfileActivity.java
@@ -12,18 +12,48 @@ import android.widget.RelativeLayout;
 import android.widget.TextView;
 
 import com.example.help2helpless.model.Client;
+import com.squareup.picasso.Picasso;
+
+import de.hdodenhof.circleimageview.CircleImageView;
 
 public class ClientProfileActivity extends AppCompatActivity {
 
+       CircleImageView image_profile;
+       TextView cname,cdeases,phone,month_income,month_cost,caddres;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_client_profile);
         setFontToActionBar();
+        initAll();
+
         Bundle bundle= getIntent().getExtras();
         Client client= bundle.getParcelable("client");
+        setValue(client);
 
 
+    }
+
+    private void setValue(Client client) {
+        cname.setText(client.getcName());
+        cdeases.setText(client.getCdisase());
+        phone.setText(client.getCnumber());
+        month_income.setText(client.getMincome());
+        month_cost.setText(client.getCmedicost());
+        caddres.setText(client.getCaddres());
+        String imageUrl="https://apps.help2helpless.com/uploads/"+client.getCphoto();
+        Picasso.get().load(imageUrl).resize(90,90).centerCrop().into(image_profile);
+
+    }
+
+    private void initAll() {
+        cname=findViewById(R.id.cname);
+        cdeases=findViewById(R.id.nm_deasese);
+        phone=findViewById(R.id.ccontact);
+        month_income=findViewById(R.id.cincome);
+        month_cost=findViewById(R.id.mcost);
+        caddres=findViewById(R.id.caddress);
+        image_profile=findViewById(R.id.cprofile_image);
 
     }
 
